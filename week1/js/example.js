f72e2b6a12a50b44f54a799d239b12bd3d4ef6aa
zlargon <zlargon1988@gmail.com>
2019-01-13 04:01:46 -0500

    [week1] refactor the example.js
    1. change all the variable from 'var' to 'const'
    2. put all the DOM elements to object 'ele'
    3. use IIFE (Immediately Invoked Function Expression) to prevent global variable

--- week1/js/example.js
+++ week1/js/example.js
@@ -1,40 +1,47 @@
+// use IIFE (Immediately Invoked Function Expression) to prevent global variable
+(() => {
+
 // Create variables for the welcome message
-var greeting = 'Howdy ';
-var name = 'Molly';
-var message = ', please check your order:';
+const greeting = 'Howdy ';
+const name = 'Molly';
+const message = ', please check your order:';
 // Concatenate the three variables above to create the welcome message
-var welcome = greeting + name + message;
+const welcome = greeting + name + message;
 
 // Create variables to hold details about the sign
-var sign = 'Montague House';
-var tiles = sign.length;
-var subTotal = tiles * 5;
-var shipping = 7;
-var grandTotal = subTotal + shipping;
+const sign = 'Montague House';
+const tiles = sign.length;
+const subTotal = tiles * 5;
+const shipping = 7;
+const grandTotal = subTotal + shipping;
+
+const ele = {}; // The object to store the elements
 
 // Get the element that has an id of greeting
-var el = document.getElementById('greeting');
+ele.greeting = document.getElementById('greeting');
 // Replace the content of that element with the personalized welcome message
-el.textContent = welcome;
+ele.greeting.textContent = welcome;
 
 // Get the element that has an id of userSign then update its contents
-var elSign = document.getElementById('userSign');
-elSign.textContent = sign;
+ele.userSign = document.getElementById('userSign');
+ele.userSign.textContent = sign;
 
 // Get the element that has an id of tiles then update its contents
-var elTiles = document.getElementById('tiles');
-elTiles.textContent = tiles;
+ele.tiles = document.getElementById('tiles');
+ele.tiles.textContent = tiles;
 
 // Get the element that has an id of subTotal then update its contents
-var elSubTotal = document.getElementById('subTotal');
-elSubTotal.textContent = '$' + subTotal;
+ele.subTotal = document.getElementById('subTotal');
+ele.subTotal.textContent = '$' + subTotal;
 
 // Get the element that has an id of shipping then update its contents
-var elShipping = document.getElementById('shipping');
-elShipping.textContent = '$' + shipping;
+ele.shipping = document.getElementById('shipping');
+ele.shipping.textContent = '$' + shipping;
 
 // Get the element that has an id of grandTotal then update its contents
-var elGrandTotal = document.getElementById('grandTotal');
-elGrandTotal.textContent = '$' + grandTotal;
+ele.grandTotal = document.getElementById('grandTotal');
+ele.grandTotal.textContent = '$' + grandTotal;
 
 // Note: textContent does not work in IE8 or earlier - see explanation on website
+
+})();
