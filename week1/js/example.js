22b3290854143db236b1550ca9c1c5095d94b9b4
zlargon <zlargon1988@gmail.com>
2019-01-13 04:09:47 -0500

    [week1] Requirement 3
    Add a conditional test that will check the variable "tileType" to see if it's "stone", "clay", or "wood".
    If it's "stone", set a new variable called "pricePerTile" to 10.
    If it's "clay", set "pricePerTile" to 5.
    If it's "wood", set pricePerTile to 7.

--- week1/js/example.js
+++ week1/js/example.js
@@ -1,6 +1,21 @@
 // use IIFE (Immediately Invoked Function Expression) to prevent global variable
 (() => {
 
+const getTilePrice = (tileType) => {
+  const TILE_TYPE_PRICE = {
+    stone: 10,
+    clay: 5,
+    wood: 7
+  };
+
+  tileType = tileType.toLowerCase();
+
+  // check whether the type exist in TILE_TYPE_PRICE or not.
+  // If it does exist, return the price of the tile type.
+  // If not, return 0.
+  return tileType in TILE_TYPE_PRICE ? TILE_TYPE_PRICE[tileType] : 0;
+}
+
 // Create variables for the welcome message
 const greeting = 'Howdy ';
 const name = 'Molly';
@@ -11,7 +26,8 @@ const welcome = greeting + name + message;
 // Create variables to hold details about the sign
 const sign = 'Montague House';
 const tiles = sign.length;
-const tileType = 'stone';     // Requirement 2
+const tileType = 'stone';                       // Requirement 2
+const pricePerTile = getTilePrice(tileType);    // Requirement 3
 const subTotal = tiles * 5;
 const shipping = 7;
 const grandTotal = subTotal + shipping;
