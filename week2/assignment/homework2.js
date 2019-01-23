3c08162abc00a7e3703fa42b4cfde0ea7dd1f82b
zlargon <zlargon1988@gmail.com>
2019-01-23 04:21:39 -0500

    [week2][assignment] requirement 1a

---
+++ week2/assignment/homework2.js
@@ -0,0 +1,7 @@
+// 1a. Create a Person class where the constructor function creates a new object with properties "name" and "pets"
+class Person {
+  constructor(name) {
+    this.name = name; // A "name" property that is initialized using a value passed to the constructor function
+    this.pets = [];   // A "pets" property that is initialized as an empty Array
+  }
+}
