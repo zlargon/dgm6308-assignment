67666edc2a633cea73880fa7a5b09e6a32bed102
zlargon <zlargon1988@gmail.com>
2019-01-23 10:28:46 -0500

    [week2][assignment] Bonus

--- week2/assignment/homework2.js
+++ week2/assignment/homework2.js
@@ -9,8 +9,8 @@ class Person {
 // 1b. Create a Pet class with a constructor function that has properties "name" and "species"
 class Pet {
   constructor(name, species) {
-    this.name = name;         // A "name" property that is initialized using a value passed to the constructor function
-    this.species = species;   // A "species" property that is initialized using a value passed to the constructor function
+    this.name = name;                             // A "name" property that is initialized using a value passed to the constructor function
+    this.species = species.trim().toLowerCase();  // A "species" property that is initialized using a value passed to the constructor function
   }
 }
 
@@ -51,9 +51,62 @@ fluffy.name = 'Mittens';
 
 // 4 & 5. Write a reportPets function that takes a Person object as a parameter and outputs to the console the person's name followed colon followed by a comma- separated list of pets with the species of pet in parentheses after each pet.
 const reportPets = (person) => {
-
   const pets = person.pets.map(pet => `${pet.name} (${pet.species})`) // use "map" to return a new string array with specified format: "pet (species)"
                           .join(', ');                                // return a new string which combines all the string elements in array with "comma separator"
 
   console.log(`${person.name}: ${pets}`);
 }
+
+// Bonus: display one person per row and as many pets per row (each in their own cell) as that person owns!
+// This is a function to render the table with people data
+const render = (table, people) => {
+  // reset the table element
+  table.innerHTML = '';
+
+  /** 1. create person header and pet header **/
+
+  // create a <tr>
+  const tr = document.createElement('tr');
+
+  // person's name <th>
+  const personHeader = document.createElement('th');
+  personHeader.textContent = "Person's Name";
+  tr.appendChild(personHeader);
+
+  // pet <th>
+  const petHeader = document.createElement('th');
+  petHeader.textContent = "Pets";
+  // set the "colspan" of the pet header by the biggest number of owning pets, and use Spread syntax to transfor array to parameters
+  petHeader.colSpan = Math.max(...people.map(person => person.pets.length));
+  tr.appendChild(petHeader);
+
+  // add <tr> to table
+  table.appendChild(tr);
+
+
+  /** 2. use for loop to create <tr>, <th>, and <td> and put the data of person and pet to cells. **/
+  for (const person of people) {
+    const tr = document.createElement('tr');
+
+    // Person Name <th>
+    const th = document.createElement('th');
+    th.textContent = person.name;
+    tr.appendChild(th);
+
+    // Pet list <td>
+    for (const pet of person.pets) {
+      const td = document.createElement('td');
+      td.textContent = `${pet.name} (${pet.species})`;
+      tr.appendChild(td);
+    }
+
+    // add <tr> to table
+    table.appendChild(tr);
+  }
+}
+
+// render the HTML
+render(
+  document.getElementById('reportTable'),   // pass the report table's DOM element
+  [mary, claire, leon]                      // pass all the people to function as an array parameter
+);
