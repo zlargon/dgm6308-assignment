9e1511ea3b9e373a0a4a01883b622e422a23e2a8
zlargon <zlargon1988@gmail.com>
2019-01-23 04:27:43 -0500

    [week2][assignment] requirement 2a & 2b

--- week2/assignment/homework2.js
+++ week2/assignment/homework2.js
@@ -13,3 +13,16 @@ class Pet {
     this.species = species;   // A "species" property that is initialized using a value passed to the constructor function
   }
 }
+
+// 2a. Using the new keyword, create a Person named Mary. Then create a Pet named Fluffy, which is of the species cat.
+const mary = new Person('Mary');
+const fluffy = new Pet('Fluffy', 'cat');
+
+// 2b. Write a function called assignPetToPerson, which takes a Pet object and a Person object as parameters.
+const assignPetToPerson = (pet, person) => {
+  // add the passed Pet object to the end of the pets array for the passed Person.
+  person.pets.push(pet);
+}
+
+// Test this function by assigning Fluffy to Mary
+assignPetToPerson(fluffy, mary);
