f30891f356a4a27a88c8e0f0dc5458e93ab03367
zlargon <zlargon1988@gmail.com>
2019-01-23 02:51:46 -0500

    [week2][workshop] show the final result

--- week2/workshop/workshop2.js
+++ week2/workshop/workshop2.js
@@ -1,16 +1,41 @@
-var gradebookData = [];
-var assignmentData = [];
+const gradebookData = [
+  // {
+  //   id: 0,
+  //   firstName: 'Adam',
+  //   lastName: 'Anders',
+  //   grades: [5, 0]
+  // },
+  // {
+  //   id: 1,
+  //   firstName: 'Beth',
+  //   lastName: 'Booker',
+  //   grades: [0, 10]
+  // }
+];
+
+const assignmentData = [
+  // {
+  //   id: 0,
+  //   assignmentName: 'Homework#1',
+  //   totalPointValue: 10
+  // },
+  // {
+  //   id: 1,
+  //   assignmentName: 'Homework#2',
+  //   totalPointValue: 20
+  // }
+];
 
 // write a class for the Student here
 
 // Assignment class
 class Assignment {
-	constructor (assignmentName, totalPointValue, assignmentData) {
-		"use strict";
-		this.assignmentName = assignmentName;
-		this.totalPointValue = totalPointValue;
-		this.id = assignmentData.length;
-	} // end Assignment constructor
+  constructor (assignmentName, totalPointValue, assignmentData) {
+    "use strict";
+    this.assignmentName = assignmentName;
+    this.totalPointValue = totalPointValue;
+    this.id = assignmentData.length;
+  } // end Assignment constructor
 } // end Assignment class
 
 // write function createNewAssignment here
@@ -23,48 +48,48 @@ class Assignment {
 // OUTPUT CODE -- DO NOT EDIT THIS WEEK!
 
 function outputGradebook(gradebookData, assignmentData) {
-    // for now, this routine will draw the gradebook once, since this isn't yet an
-    // interactive page
-    var i, j;
+  // for now, this routine will draw the gradebook once, since this isn't yet an
+  // interactive page
+  var i, j;
 
-    // set up our assignment header column in gradebook
-    var gradeHeader = document.createElement('tr');
-    var gradeTable = document.getElementById('gradebook');
-    var totalPoints = 0;
+  // set up our assignment header column in gradebook
+  var gradeHeader = document.createElement('tr');
+  var gradeTable = document.getElementById('gradebook');
+  var totalPoints = 0;
 
-    gradeHeader.appendChild(document.createElement('td')); // firstName blank
-    gradeHeader.appendChild(document.createElement('td')); // lastName blank
-    gradeHeader.appendChild(document.createElement('td')); // total percentage blank
-    for (i = 0; i < assignmentData.length; i = i + 1) {
-        var assignmentHeader = document.createElement('td');
-        assignmentHeader.appendChild(document.createTextNode(assignmentData[i].assignmentName + " (" + assignmentData[i].totalPointValue + ")"));
-        gradeHeader.appendChild(assignmentHeader);
-        totalPoints = totalPoints + assignmentData[i].totalPointValue;
+  gradeHeader.appendChild(document.createElement('td')); // firstName blank
+  gradeHeader.appendChild(document.createElement('td')); // lastName blank
+  gradeHeader.appendChild(document.createElement('td')); // total percentage blank
+  for (i = 0; i < assignmentData.length; i = i + 1) {
+    var assignmentHeader = document.createElement('td');
+    assignmentHeader.appendChild(document.createTextNode(assignmentData[i].assignmentName + " (" + assignmentData[i].totalPointValue + ")"));
+    gradeHeader.appendChild(assignmentHeader);
+    totalPoints = totalPoints + assignmentData[i].totalPointValue;
+  }
+  gradeTable.appendChild(gradeHeader);
+
+  // output students from gradebook
+  for (i = 0; i  < gradebookData.length; i = i + 1) {
+    var studentRow = gradeTable.appendChild(document.createElement('tr'));
+    var id = studentRow.appendChild(document.createElement('td'));
+    id.appendChild(document.createTextNode(gradebookData[i].id));
+    var studentName = studentRow.appendChild(document.createElement('td'));
+    studentName.appendChild(document.createTextNode(gradebookData[i].firstName +
+                            " " + gradebookData[i].lastName));
+    // we are creating a place for our student % but we don't know the value yet!
+    var studentPercent = studentRow.appendChild(document.createElement('td'));
+    var studentRunningTotal = 0;
+    for (j = 0; j < gradebookData[i].grades.length; j = j + 1) {
+      var gradeData = studentRow.appendChild(document.createElement('td'));
+      var assignmentScore = gradebookData[i].grades[j];
+      gradeData.appendChild(document.createTextNode(assignmentScore));
+      studentRunningTotal = studentRunningTotal + assignmentScore;
     }
-    gradeTable.appendChild(gradeHeader);
+    // now that we have a running total for this student we can calculate a percentage
+    var studentPercentage = (studentRunningTotal/totalPoints) * 100;
 
-    // output students from gradebook
-    for (i = 0; i  < gradebookData.length; i = i + 1) {
-        var studentRow = gradeTable.appendChild(document.createElement('tr'));
-        var id = studentRow.appendChild(document.createElement('td'));
-        id.appendChild(document.createTextNode(gradebookData[i].id));
-        var studentName = studentRow.appendChild(document.createElement('td'));
-        studentName.appendChild(document.createTextNode(gradebookData[i].firstName +
-                                                        " " + gradebookData[i].lastName));
-        // we are creating a place for our student % but we don't know the value yet!
-        var studentPercent = studentRow.appendChild(document.createElement('td'));
-        var studentRunningTotal = 0;
-        for (j = 0; j < gradebookData[i].grades.length; j = j + 1) {
-            var gradeData = studentRow.appendChild(document.createElement('td'));
-            var assignmentScore = gradebookData[i].grades[j];
-            gradeData.appendChild(document.createTextNode(assignmentScore));
-            studentRunningTotal = studentRunningTotal + assignmentScore;
-        }
-        // now that we have a running total for this student we can calculate a percentage
-        var studentPercentage = (studentRunningTotal/totalPoints) * 100;
-
-        studentPercent.appendChild(document.createTextNode(studentPercentage.toFixed(1)));
-    }
+    studentPercent.appendChild(document.createTextNode(studentPercentage.toFixed(1)));
+  }
 
 }
 
