cbbe42aa1c0d71ad7da284e5f9b603f699718417
zlargon <zlargon1988@gmail.com>
2019-01-30 21:27:58 -0500

    [week4][workshop] task 2: create <tr> at the begining of the outer for-loop

--- week4/workshop/buttons.js
+++ week4/workshop/buttons.js
@@ -1,10 +1,16 @@
-const theTable = document.getElementById("myTable");
+const theTable = document.getElementById('myTable');
 const numRows = 3;
 const numColumns = 3;
 
 // task 1: Create a nested set of loops. The outer loop, i, should count from 0 to (but not including) numRows.
 // The inner loop, j, should count from 0 to (but not including) numColumns.
 for (let i = 0; i < numRows; i++) {
+
+  // task 2: Each time through the outer loop i, before inner loop j begins, use document.createElement() to create a new table row.
+  // Assign the newly created element to variable theRow. Then make theRow a child of theTable using appendChild().
+  const theRow = document.createElement('tr');
+  theTable.appendChild(theRow);
+
   for (let j = 0; j < numColumns; j++) {
 
   }
