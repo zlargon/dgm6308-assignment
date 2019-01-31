2e6f81a5d1a7110ddc8cbf6e65ef2d29440ee25a
zlargon <zlargon1988@gmail.com>
2019-01-30 21:23:46 -0500

    [week4][workshop] task1: create nested for-loop

--- week4/workshop/buttons.js
+++ week4/workshop/buttons.js
@@ -1,3 +1,11 @@
-var theTable = document.getElementById("myTable");
-var numRows = 3;
-var numColumns = 3;
+const theTable = document.getElementById("myTable");
+const numRows = 3;
+const numColumns = 3;
+
+// task 1: Create a nested set of loops. The outer loop, i, should count from 0 to (but not including) numRows.
+// The inner loop, j, should count from 0 to (but not including) numColumns.
+for (let i = 0; i < numRows; i++) {
+  for (let j = 0; j < numColumns; j++) {
+
+  }
+}
