1f280dd6a469e2234803985c1bc2eff3bbc2e776
zlargon <zlargon1988@gmail.com>
2019-02-14 02:06:33 -0500

    [week5][homework] show the submit buttons when the fields are not empty

--- week5/homework/hmwk5.js
+++ week5/homework/hmwk5.js
@@ -5,21 +5,22 @@ const panels = document.getElementById('panel').children;
 const login = {
   username: document.getElementById('login_username'),
   password: document.getElementById('login_password'),
-  submit:   document.getElementById('login_submit')
+  button:   document.getElementById('login_submit')
 };
 
 const signup = {
   username: document.getElementById('signup_username'),
   password: document.getElementById('signup_password'),
   verify:   document.getElementById('signup_verify_password'),
-  submit:   document.getElementById('signup_submit')
+  button:   document.getElementById('signup_submit')
 };
 
 const guest = {
-  login: document.getElementById('guest_login')
+  button: document.getElementById('guest_login')
 }
 
 const show = (ele, bool) => {
+  // add class 'hide' if bool is falsa; otherwise, remove class 'hide'
   ele.classList[bool ? 'remove' : 'add']('hide');
 }
 
@@ -27,15 +28,29 @@ const render = () => {
   // get mode: login / signup / guest
   const mode = document.querySelector('input[name="mode"]:checked').value;
 
+  // 2abc. show the panel according the mode
   for (const panel of panels) {
-    // add class 'hide' if mode is not matched; otherwise, remove class 'hide'
     show(panel, panel.getAttribute('mode') === mode);
   }
+
+  // 3ab. login button: show the button when the fields are not blank
+  show(login.button,
+    login.username.value.length !== 0 &&
+    login.password.value.length !== 0
+  );
+
+  // 3ab. signup button: show the button when the fields are not blank
+  show(signup.button,
+    signup.username.value.length !== 0 &&
+    signup.password.value.length !== 0 &&
+    signup.verify.value.length   !== 0
+  );
 }
 
 // when the form be changed, invoke render()
 const form = document.querySelector('form');
 form.addEventListener('change', render);
+form.addEventListener('keyup', render);
 
 // init the mode panel at first time
 render();
