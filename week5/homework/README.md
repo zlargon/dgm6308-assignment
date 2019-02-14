5852f9ccc980f19d6929006ea2bfdf466fc52c23
zlargon <zlargon1988@gmail.com>
2019-02-14 00:23:51 -0500

    [week5][homework] click the radio button to change the login/signup/guest mode

--- week5/homework/README.md
+++ week5/homework/README.md
@@ -1,3 +1,3 @@
 # Week5 Homework
 
-https://zlargon.github.io/dgm6308-assignment/week5/homework/hmwk5.html
+https://zlargon.github.io/dgm6308-assignment/week5/homework/
--- week5/homework/hmwk5.js
+++ week5/homework/hmwk5.js
@@ -1,32 +1,42 @@
-// get login & password from the user
-(function() {
-	var form = document.getElementById('login');
-	
-	addEvent(form, 'submit', function(e) {
-		e.preventDefault();
-		var elements = this.elements;
-		var username = elements.username.value;
-		var msg = 'Welcome ' + username;
-		document.getElementById('main').textContent = msg;
-	});
-	
-	// changing password input field to be visible or not	
-	var pwd = document.getElementById('pwd');
-	var chk = document.getElementById('showPwd');
-	
-	addEvent(chk, 'change', function(e){
-		var target= e.target || e.srcElement;
-		console.log(" pwd = " + pwd);
-		try{
-			if (target.checked) {
-				pwd.type = 'text';
-			} else {
-				pwd.type = 'password';
-			}
-		} catch(error) {
-			alert('This browser cannot switch type');
-		}
-	});
-	
-	
-}());
+// elements
+const radios = document.querySelectorAll('input[name="mode"]');
+const panels = document.getElementById('panel').children;
+
+const login = {
+  username: document.getElementById('login_username'),
+  password: document.getElementById('login_password'),
+  submit:   document.getElementById('login_submit')
+};
+
+const signup = {
+  username: document.getElementById('signup_username'),
+  password: document.getElementById('signup_password'),
+  verify:   document.getElementById('signup_verify_password'),
+  submit:   document.getElementById('signup_submit')
+};
+
+const guest = {
+  login: document.getElementById('guest_login')
+}
+
+const show = (ele, bool) => {
+  ele.classList[bool ? 'remove' : 'add']('hide');
+}
+
+const render = () => {
+  // get mode: login / signup / guest
+  const mode = document.querySelector('input[name="mode"]:checked').value;
+
+  for (const panel of panels) {
+    // add class 'hide' if mode is not matched; otherwise, remove class 'hide'
+    show(panel, panel.getAttribute('mode') === mode);
+  }
+}
+
+// when the form be changed, invoke render()
+const form = document.querySelector('form');
+form.addEventListener('change', render);
+
+// init the mode panel at first time
+render();
+
