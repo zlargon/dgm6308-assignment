25bf8660b1c3dedac1b8cd909addc582056f364d
zlargon <zlargon1988@gmail.com>
2019-02-19 23:41:11 -0500

    [week6][workshop] doubleClickStart

--- week6/workshop/doubleClickStart.js
+++ week6/workshop/doubleClickStart.js
@@ -1,14 +1,31 @@
-$(function() {
-	var DELAY = 700, clicks = 0, timer = null;
-	
-	$("a").on("click", function(e){	
-		
-		// your code goes here
-		
-	 })
-	.on("dblclick", function(e){
-        e.preventDefault();  //cancel system double-click event
-    });
+$(() => {
+  const DELAY = 700;
+  let clicks = 0, timer = null;
 
-	
+  $('a').on('click', (e) => {
+    clicks++; // increase clicks
+
+    // leave function because the timer is already started
+    if (timer !== null) {
+      return;
+    }
+
+    // set timer
+    timer = setTimeout(() => {
+      // alert the result
+      alert(clicks > 1 ? 'Double Click' : 'Single Click');
+
+      // reset clicks
+      clicks = 0;
+
+      // reset timer
+      clearTimeout(timer);
+      timer = null;
+
+    }, DELAY);
+
+  })
+  .on('dblclick', (e) => {
+    e.preventDefault(); // cancel system double-click event
+  });
 });
