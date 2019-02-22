49d777a5074b3bb89b74525471e2c8b91f5e4a3e
zlargon <zlargon1988@gmail.com>
2019-02-22 16:05:39 -0500

    [week7] init the assignment folder

---
+++ week7/README.md
@@ -0,0 +1,3 @@
+# Week7 Assignment
+
+https://zlargon.github.io/dgm6308-assignment/week7/index.html
---
+++ week7/index.js
@@ -0,0 +1,5 @@
+/* For exercise 1:
+	in this file, attach an event handler to the image object you named in your HTML file
+	(using getElementbyId and referring to the ID that you set in the HTML)
+	The event handler should handle the "click" event and should simply swap the
+	handle_up.jpg image with the handle_down.jpg image */
