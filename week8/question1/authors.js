1333153e4ef4b0c28d4e235c01c692fb9f5513f3
zlargon <zlargon1988@gmail.com>
2019-03-01 17:31:25 -0500

    [week8] init questions 1 ~ 4

---
+++ week8/question1/authors.js
@@ -0,0 +1,22 @@
+var authors = ['Ernest Hemingway', 'Charlotte Bronte', 'Louisa May Alcott', 'Charles Dickens'];
+
+var titles = ['The Old Man and the Sea', 'Jane Eyre', 'Little Women', 'Oliver Twist'];
+ 
+
+// write this function to take 2 strings, an author and a title and add them to the BEGINNING of the respective arrays.
+function addToArrays(myAuthor, myTitle) {
+	
+}
+
+// now call the function with your author and title -- you can make up names!
+
+
+// write the function to create the table and add the authors and their titles
+function outputTable(authors, titles) {
+	
+}
+
+// now invoke the function: 
+outputTable(authors, titles);
+
+// Finally add the 2 paragraphs
---
+++ week8/question2/radios.js
@@ -0,0 +1,26 @@
+(function(){
+  var form, options, other, otherText, hide;           // Declare variables
+  // create a select box in JS from an array
+  var array = ['Friend', 'Email', 'Career Fair', 'Referal']; 
+  
+  form      = document.getElementById('myForm');    // Get the form
+  
+  // get the radio buttons next
+  options = form.elements.heard;
+  
+  // hide the select boxes
+  
+  
+  // now in a loop add an event listener to each of the options - do not use the text book utilities
+  
+  
+  // create the select box
+  
+  
+  // write a function to check and see if the radio button selection has changed
+  // if other is selected, display the select box; otherwise hide the select box
+  function radioChanged() {
+	  
+	  
+  }
+}());
